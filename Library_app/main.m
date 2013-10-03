b81fb623e27bc9b54f049caccd1d5c48de680e5f
Bennett Lin <bennettslin@gmail.com>
2013-10-02 19:40:44 -0700

    Initial Commit

---
+++ Library_app/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Library_app
+//
+//  Created by Bennett Lin on 10/2/13.
+//  Copyright (c) 2013 Bennett Lin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
